d3d4c66953ce6815ce85bc68fe927ae94c3a757d
Akshit Chaudhary <140873718+chaudharyakshit@users.noreply.github.com>
2026-05-08 12:11:17 +0530

    added story model with title url points author and postedAt

---
+++ server/models/Story.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const storySchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  url: {
+    type: String,
+    default: '',
+  },
+  points: {
+    type: Number,
+    default: 0,
+  },
+  author: {
+    type: String,
+    default: 'unknown',
+  },
+  postedAt: {
+    type: String,
+    default: '',
+  },
+  scrapedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('Story', storySchema);
